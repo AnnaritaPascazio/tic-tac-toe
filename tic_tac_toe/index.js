e210845e0c12edcc512a3528b7b875d8a25a9464
Alexismoon23 <128156806+Alexismoon23@users.noreply.github.com>
2024-12-10 17:14:45 +0100

    logica-2

--- tic_tac_toe/index.js
+++ tic_tac_toe/index.js
@@ -10,9 +10,23 @@ somma();*/
 
 let history= [Array(9).fill(null)];
 let currentMove = 0;
+let xIsNext = currentMove % 2 == 0;
+let currentSquares = history(currentMove);
 
 console.log('history',history);
 function handleClick(button)
 {
     console.log('cliccato',button);
+
+    const nextSquares = currentSquares.slice();
+    if(xIsNext)
+    {
+        nextSquares[button -1 ] = "X";
+    }else 
+    {
+        nextSquares[button -1] = '0';
+    }
+    console.log('nextSquares',nextSquares);
+    const square = document.getElementById(button);
+    square.innerText = nextSquares[button -1];
 }
